e65da684b525d18eadcb00baa01443080f94fc94
GeoSnipes <geovanni.fortisyute@gmail.com>
2018-02-17 12:38:31 -0600

    update

--- src/getplace.py
+++ src/getplace.py
@@ -1,18 +1,21 @@
 """Note, this was built taking into account the base input folder only contains other folders """
 import os
 from random import randint
-from shutil import copy2
+from shutil import copy2, move
 
-def getFiles(directory):
+def getFiles(base, source, folder):
     count = 0
-    files = os.listdir(directory)   
+    dest = base + "\\imagestest\\" + folder
+    files = os.listdir(source)
+    if not os.path.exists(dest):
+        os.makedirs(dest)
     #takes 3 files from each folder
     while count < 3:
         randomNum = randint(0, len(files)-1)
-        copy2(directory + "\\" + files[randomNum],"D:\\Users\\Geovanni\\Downloads\\food-101.tar\\food-101\\food")
+        move(source + "\\" + files[randomNum], dest)    #or copy2 to copy
         count+=1
 
 
-path = "D:\\Users\\Geovanni\\Downloads\\food-101.tar\\food-101\\images" #hardcoded here, use sys.argv or your own implementation to be more dynamic
-for folder in os.listdir(path):
-    getFiles(path+"\\"+folder)
+base = "D:\\Users\\Geovanni\\Downloads\\food-101.tar\\food-101" #hardcoded here, use sys.argv or your own implementation to be more dynamic
+for folder in os.listdir(base+"\\images"):
+    getFiles(base, base+"\\images\\"+folder,folder)
